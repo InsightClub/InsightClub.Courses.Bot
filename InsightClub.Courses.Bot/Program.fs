ba91cca8e7515f130da2d995692b330b55d69758
Reenuay <reenuay777@gmail.com>
2021-11-11 23:36:38 +0400

    Add Config.json

--- InsightClub.Courses.Bot/Program.fs
+++ InsightClub.Courses.Bot/Program.fs
@@ -1,6 +1,10 @@
 module InsightClub.Courses.Bot.Program
 
+open InsightClub.Courses.Bot.Config
+
 
 [<EntryPoint>]
 let main _ =
+  let config = Config.load "Config.json"
+
   0 // return an integer exit code
